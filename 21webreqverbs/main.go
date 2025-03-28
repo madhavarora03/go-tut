dd8d920239fd81abc4be17d013073740d22af20a
madhav <eragon.cube@outlook.com>
2025-03-28 09:32:14 +0530

    POST Json data in go

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	fmt.Println("Welcome to web verbs")
 
-	PerformGetRequest()
+	//PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
 func PerformGetRequest() {
@@ -35,4 +36,29 @@ func PerformGetRequest() {
 	fmt.Println(responseString.String())
 
 	//fmt.Println(string(content)) // can also use this
+}
+
+func PerformPostJsonRequest() {
+	myUrl := "http://localhost:8000/post"
+
+	//	fake json payload
+	requestBody := strings.NewReader(`
+		{
+			"courseName": "Let's go with golang",
+			"price": 0,
+			"platform": "youtube.com"
+		}
+	`)
+
+	response, err := http.Post(myUrl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 }
