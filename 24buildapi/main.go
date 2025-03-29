15c17ceb153138ed44189907cf8d34252619e64f
madhav <eragon.cube@outlook.com>
2025-03-29 10:35:53 +0530

    GET course by id

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -42,4 +43,23 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses...")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//	grab id from request
+	params := mux.Vars(r)
+
+	//	loop through courses, find matching id and return response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No Course found with given id!")
+	return
 }
