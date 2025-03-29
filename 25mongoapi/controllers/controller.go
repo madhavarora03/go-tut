2d928cb9eb9c85c5c9a969649b8f5acc23ee721b
madhav <eragon.cube@outlook.com>
2025-03-29 17:25:15 +0530

    Update record mongodb

--- 25mongoapi/controllers/controller.go
+++ 25mongoapi/controllers/controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	model "github.com/madhavarora03/mongoapi/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"log"
@@ -44,4 +45,18 @@ func insertOneMovie(movie model.Netflix) {
 	}
 
 	fmt.Println("Inserted one movie with id:", inserted.InsertedID)
+}
+
+func updateOneMovie(movieId string) {
+	id, _ := bson.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Modified count:", result.ModifiedCount)
 }
